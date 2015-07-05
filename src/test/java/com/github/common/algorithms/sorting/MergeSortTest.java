2b608b7348bcd73f9a9a73f23a40432a14b0691e
sarovios <sot.beis@gmail.com>
2015-07-05 16:58:03 +0100

    add test cases

--- src/test/java/com/github/common/algorithms/sorting/MergeSortTest.java
+++ src/test/java/com/github/common/algorithms/sorting/MergeSortTest.java
@@ -3,38 +3,52 @@ package com.github.common.algorithms.sorting;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
+//TODO: add test cases to merge function
 public class MergeSortTest {
 
-    private MergeSort mergeSort;
+    MergeSort mergeSort;
 
-//    @Before
-//    public void setUp() {
-//        this.mergeSort = new MergeSort();
-//    }
-//
-//    @Test
-//    public void testMergeEvenSizeArrays() {
-//        int[] arr1 ={1, 2, 4, 6};
-//        int[] arr2 = {3, 5, 7, 8};
-//        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8};
-//        assertArrayEquals(expected, mergeSort.merge(arr1, arr2, 8));
-//    }
-//
-//    @Test
-//    public void testMergeOddSizeArrays() {
-//        int[] arr1 ={1, 2, 4, 6};
-//        int[] arr2 = {3, 5, 7, 8, 10, 15, 50};
-//        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 10, 15, 50};
-//        assertArrayEquals(expected, mergeSort.merge(arr1, arr2, 11));
-//    }
+    @Before
+    public void setUp() {
+        mergeSort = new MergeSort();
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testMergeSortNullArray() {
+        int[] array = null;
+        mergeSort.sort(array);
+    }
+
+    @Test
+    public void testMergeSortZeroValuesArray() {
+        int[] array = {};
+        int[] expected = {};
+        mergeSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testMergerSortOneValueArray() {
+        int[] array = {2};
+        int[] expected = {2};
+        mergeSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
 
     @Test
     public void testMergeSortEvenSizeArray() {
         int[] array = {2, 4, 1, 6, 8, 5, 3, 7};
         int[] expected = {1, 2, 3, 4, 5, 6, 7, 8};
-        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testMergeSortOddSizeArray() {
+        int[] array = {2, 4, 1, 6, 8, 5, 3, 7, 15};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 15};
         mergeSort.sort(array);
         assertArrayEquals(expected, array);
     }
