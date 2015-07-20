091b5bdc0d83f6246ef2ca10dd1db70e07311c96
sarovios <sot.beis@gmail.com>
2015-07-20 20:56:22 +0100

    add more tests

--- src/test/java/com/github/common/algorithms/sorting/QuickSortTet.java
+++ src/test/java/com/github/common/algorithms/sorting/QuickSortTet.java
@@ -1,9 +1,15 @@
 package com.github.common.algorithms.sorting;
 
+import com.github.common.algorithms.problem.CountInversions;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class QuickSortTet {
 
@@ -64,6 +70,26 @@ public class QuickSortTet {
     public void testQuickSortOddSizeWithDuplicatesArray() {
         int[] array = {2, 4, 1, 6, 8, 5, 3, 7, 4};
         int[] expected = {1, 2, 3, 4, 4, 5, 6, 7, 8};
+        quickSort.sort(array);
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testQuickSortBigArray() {
+        int[] array = new int[10000];
+        int[] expected = new int[10000];
+        int i = 0;
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("comparisons.txt").getFile());
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                int val =  scanner.nextInt();
+                array[i++] = val;
+                expected[i-1] = i;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         quickSort.sort(array);
         assertArrayEquals(expected, array);
     }
