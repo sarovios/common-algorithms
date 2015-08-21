757b6bf0fcf95c1a9ba29a0ad91e282585388404
Sotiris Beis <sotirios.beis@stratifiedmedical.com>
2015-08-21 18:15:14 +0100

    implement karger min cut algorithm - wip

--- src/main/java/com/github/common/algorithms/problem/KargerMinCut.java
+++ src/main/java/com/github/common/algorithms/problem/KargerMinCut.java
@@ -6,19 +6,29 @@ import java.util.*;
 public class KargerMinCut {
 
     private Random random = new Random();
-    private Map<Integer, List<Integer>> contractedAdjList;
 
-    public void findMinCut(Map<Integer, List<Integer>> adjList, int iterations) {
-        contractedAdjList = new HashMap<>(adjList);
-        for (int i = 0; i < iterations; i++) {
-            randomizedContraction(contractedAdjList);
+    public void findMinCut(List<int[]> edgeList) {
+        for (int i = 0; i < 1; i++) {
+            List<int[]> res = randomizedContraction(edgeList);
+            System.out.println();
         }
     }
 
-    public void randomizedContraction(Map<Integer, List<Integer>> contractedAdjList) {
-        int node = random.nextInt(contractedAdjList.size()) + 1;
-        System.out.println(node);
-
+    public List<int[]> randomizedContraction(List<int[]> edgeList) {
+        while (edgeList.size() > 2) {
+            int edgeId = random.nextInt(edgeList.size());
+            int[] edge = edgeList.get(edgeId);
+            int start = edge[0];
+            int end = edge[1];
+            Iterator<int[]> iter = edgeList.iterator();
+            while (iter.hasNext()) {
+                int[] e = iter.next();
+                if (e[0] == end) e[0] = start;
+                if (e[1] == end) e[1] = start;
+                if ( (e[0] == start) && (e[1] == end) ) iter.remove();
+            }
+        }
+        return edgeList;
     }
 
 }
--- src/test/java/com/github/common/algorithms/problem/KargerMinCutTest.java
+++ src/test/java/com/github/common/algorithms/problem/KargerMinCutTest.java
@@ -3,8 +3,6 @@ package com.github.common.algorithms.problem;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 
 public class KargerMinCutTest {
@@ -17,25 +15,44 @@ public class KargerMinCutTest {
     }
 
     @Test
-    public void kargerMinCutTest() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("kargerMinCut.txt").getFile());
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String[] line = scanner.nextLine().split("\t");
-                int nodeId = Integer.valueOf(line[0]);
-                List<Integer> edges = new ArrayList<Integer>();
-                for (int i = 1; i < line.length; i++) {
-                    edges.add(Integer.valueOf(line[i]));
-                }
-                adjList.put(nodeId, edges);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        kargerMinCut.findMinCut(adjList, adjList.size() * adjList.size());
+    public void kargerMinCutSmallTest() {
+//        Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>() {{
+//            put(1, new ArrayList<Integer>(){{ add(3); add(2); }});
+//            put(2, new ArrayList<Integer>(){{ add(1); add(3); add(4);}});
+//            put(3, new ArrayList<Integer>() {{ add(1); add(2); add(4);}});
+//            put(4, new ArrayList<Integer>() {{ add(3); add(2); }});
+//        }};
+        List<int[]> edgeList = new ArrayList<int[]>() {{
+            add(new int[]{1, 2});
+            add(new int[]{1, 3});
+            add(new int[]{2, 3});
+            add(new int[]{2, 4});
+            add(new int[]{3, 4});
+        }};
 
+        kargerMinCut.findMinCut(edgeList);
     }
+
+//    @Test
+//    public void kargerMinCutTest() {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("kargerMinCut.txt").getFile());
+//        Map<Integer, List<Integer>> adjList = new HashMap<>();
+//        try (Scanner scanner = new Scanner(file)) {
+//            while (scanner.hasNextLine()) {
+//                String[] line = scanner.nextLine().split("\t");
+//                int nodeId = Integer.valueOf(line[0]);
+//                List<Integer> edges = new ArrayList<Integer>();
+//                for (int i = 1; i < line.length; i++) {
+//                    edges.add(Integer.valueOf(line[i]));
+//                }
+//                adjList.put(nodeId, edges);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        kargerMinCut.findMinCut(adjList, adjList.size() * adjList.size());
+//
+//    }
 
 }
